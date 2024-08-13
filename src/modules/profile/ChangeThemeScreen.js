e596a63c6eafc518c3b96f9bde630ad58f9fd9ca
hamza <hamzahalilovic.12@gmail.com>
2024-08-13 18:25:16 +0200

    sad

--- src/modules/profile/ChangeThemeScreen.js
+++ src/modules/profile/ChangeThemeScreen.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import {Text, View, Button} from '@gluestack-ui/themed-native-base';
-
-import {useThemeContext} from '../../theme/ThemeContext';
+import {Box, Text, Switch, useTheme} from '@gluestack-ui/themed-native-base';
 
 const ChangeThemeScreen = () => {
-  const {toggleTheme} = useThemeContext();
-
   return (
-    <View style={{flex: 1, padding: 16, backgroundColor: 'background'}}>
-      <Text fontSize={24} fontWeight={600} mb={4} color="text">
-        Change theme Screen
-      </Text>
-      <Button onPress={toggleTheme} mt={4}>
-        Toggle Theme
-      </Button>
-    </View>
+    <Box flex={1} alignItems="center" justifyContent="center">
+      <Text mb={4}>Current Theme: </Text>
+    </Box>
   );
 };
 
